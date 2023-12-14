b37ce86bbc3ec8ac12bbda01971e5f66e2ad0223
Koniukhova Sofia <sovunja1712@gmail.com>
2023-12-14 17:21:54 +0300

    Main table initial markup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="table">
+        <div className="header">
+          <p> </p>
+          <p>Name</p>
+          <p>Type</p>
+          <p>Current Progress</p>
+          <p>Size</p>
+          <p>Progress Bar</p>
+          <p> </p>
+        </div>
+        <ul className="line">
+          <li>
+            <button className="button">➕</button>
+          </li>
+          <li>JS course</li>
+          <li>course</li>
+          <li>15</li>
+          <li>20</li>
+          <li>
+            25%
+            <div className="progress">
+              <div className="percentage"> </div>
+            </div>
+          </li>
+          <li>
+            <button className="button">📃</button>
+          </li>
+        </ul>
+        <ul className="line">
+          <li>
+            <button className="button">➕</button>
+          </li>
+          <li>HTML + CSS course</li>
+          <li>course</li>
+          <li>30</li>
+          <li>30</li>
+          <li>Finished 🎉</li>
+          <li>
+            <button className="button">📃</button>
+          </li>
+        </ul>
+        <ul className="line">
+          <li>
+            <button className="button">➕</button>
+          </li>
+          <li>Typescript</li>
+          <li>book</li>
+          <li>0</li>
+          <li>276 pages</li>
+          <li>Not started</li>
+          <li>
+            <button className="button">📃</button>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
