21436c56f9675ab7d3a77ac95b2048fdf21e555b
Koniukhova Sofia <sovunja1712@gmail.com>
2024-09-18 17:09:09 +0300

    Collect data with react-hook-form

--- src/features/skills/NewItemModal.jsx
+++ src/features/skills/NewItemModal.jsx
@@ -1,101 +1,102 @@
 import Button from "../../ui/Button";
-import Input from "../../ui/Input";
 import ModalHeading from "../../ui/ModalHeading";
+import { useForm } from "react-hook-form";
 
 import style from "./NewItemModal.module.css";
+import inputStyle from "../../ui/Input.module.css";
 
 function NewItemModal({ onCloseModal }) {
-  const handleSubmit = () => {};
+  const { register, handleSubmit, reset, formState } = useForm();
+  const { errors } = formState;
+
+  function onSubmit(data) {
+    console.log("onSubmit", data);
+  }
+
+  function onError(errors) {
+    console.error(errors);
+  }
 
   return (
     <div className={style.wrapper}>
       <ModalHeading>Creating new item</ModalHeading>
 
-      <form
-        className={style.form}
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleSubmit();
-        }}
-      >
+      <form className={style.form} onSubmit={handleSubmit(onSubmit, onError)}>
         <label className={style.label}>
           Name
-          <Input
+          <input
             type="text"
             placeholder="Name"
-            value={""}
-            disabled={false}
-            name="name"
-            onChange={(e) => {
-              console.log(e);
-            }}
+            id="name"
+            className={inputStyle.input}
+            {...register("name", { required: "This field is required" })}
           />
+          {errors.name?.message && (
+            <p className={style.error}>{errors.name.message}</p>
+          )}
         </label>
         <label className={style.label}>
           Size
-          <Input
+          <input
             type="number"
             step="0.01"
             placeholder="Size"
-            value={""}
-            disabled={false}
-            name="size"
-            onChange={(e) => {
-              if (
-                !isNaN(e.target.value) ||
-                e.target.value === "," ||
-                e.target.value === "."
-              ) {
-                //   setNewProgress(e.target.value);
-                //   setError("");
-              }
-            }}
+            id="size"
+            className={inputStyle.input}
+            {...register("size", {
+              required: "This field is required",
+              min: {
+                value: 0.01,
+                message: "Size cannot be less than 0.01",
+              },
+            })}
           />
+          {errors.size?.message && (
+            <p className={style.error}>{errors.size.message}</p>
+          )}
         </label>
         <label className={style.label}>
           Type
-          <Input
+          <input
             type="text"
             placeholder="Type (book, course, etc.)"
-            value={""}
-            disabled={false}
-            name="type"
-            onChange={(e) => {
-              console.log(e);
-            }}
+            id="type"
+            className={inputStyle.input}
+            {...register("type", { required: "This field is required" })}
           />
+          {errors.type?.message && (
+            <p className={style.error}>{errors.type.message}</p>
+          )}
         </label>
         <label className={style.label}>
           Counter word
-          <Input
+          <input
             type="text"
             placeholder="Counter word (page, lesson, etc.)"
-            value={""}
-            disabled={false}
-            name="counterWord"
-            onChange={(e) => {
-              console.log(e);
-            }}
+            id="counterWord"
+            className={inputStyle.input}
+            {...register("counterWord", { required: "This field is required" })}
           />
+          {errors.counterWord?.message && (
+            <p className={style.error}>{errors.counterWord.message}</p>
+          )}
         </label>
 
         <label className={style.label}>
           Info
-          <Input
+          <input
             type="text"
             placeholder="Any additional info"
-            value={""}
-            disabled={false}
-            name="info"
-            onChange={(e) => {
-              console.log(e);
-            }}
+            id="info"
+            className={inputStyle.input}
           />
+          {errors.info?.message && (
+            <p className={style.error}>{errors.info.message}</p>
+          )}
         </label>
 
         <Button disabled={false}>Create</Button>
       </form>
-      {/* {error && <p className={style.error}>ERROR</p>} */}
     </div>
   );
 }
