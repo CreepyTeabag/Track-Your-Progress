520c64e7c83411a43add83fa29cb1f5f2b94d794
Koniukhova Sofia <sovunja1712@gmail.com>
2025-02-06 16:32:09 +0300

    Add tiny loader version

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,8 +1,8 @@
 import style from "./Loader.module.css";
 
-function Loader() {
+function Loader({ size }) {
   return (
-    <div className={style.wrapper}>
+    <div className={`${style.wrapper} ${size === "tiny" ? style.tiny : ""}`}>
       <span className={style.loader}></span>
     </div>
   );
